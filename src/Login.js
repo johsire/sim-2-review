5564655d288c663ed498e4b242f75160e216fc30
Josiah Maina <josiah5115@gmail.com>
2018-07-19 18:57:40 -0600

    added dashboard

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Link } from 'react-router-dom'
 
 export default class Login extends React.Component {
   constructor() {
@@ -14,11 +15,11 @@ export default class Login extends React.Component {
     return (
       <div>
         <div>Login</div>
-        <button onClick={() => this.handleLogin("admin")}>I'm an Admin</button>
-        <button onClick={() => this.handleLogin("student")}>
+        <Link to='/dashboard'><button onClick={() => this.handleLogin("admin")}>I'm an Admin</button></Link>
+        <Link to='/dashboard'><button onClick={() => this.handleLogin("student")}>
           I'm a Student
-        </button>
+        </button></Link>
       </div>
     );
   }
-}
+};
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import Student from "./student/Student";
 import Admin from "./admin/Admin";
 import Login from './Login';
+import Dashboard from './Dashboard';
 
 export default (
   <Switch>
     <Route exact path="/" component={Login} />
     <Route path="/student" component={Student} />
     <Route path="/admin" component={Admin} />
+    <Route path="/dashboard" component={Dashboard} />
   </Switch>
 );
